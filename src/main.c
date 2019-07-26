eccdb2854c84c3cc26aabb3b5549fed05a8fd3c3
pestbuns <ant.one.dugast@gmail.com>
2019-07-26 12:59:57 +0000

    Cleanup + use recvmsg instead of recv

--- src/main.c
+++ src/main.c
@@ -7,13 +7,10 @@
 #include <linux/netlink.h>
 
 
-#define MSG_LEN 512
-
-
 static int nl_socket_save = -1;
 static void signal_handler(__attribute__((unused)) int signum)
 {
-    printf("\nnetlink_socket[%d]:closed\n", nl_socket_save);
+    printf("Netlink socket[%d]:closed\n", nl_socket_save);
     close(nl_socket_save);
     exit(EXIT_SUCCESS);
 }
@@ -21,58 +18,55 @@ static void signal_handler(__attribute__((unused)) int signum)
 
 static int get_netlink_socket()
 {
-    struct sockaddr_nl src_addr = {0};
-
-    src_addr.nl_family = AF_NETLINK;
-    src_addr.nl_pid = getpid();
-    src_addr.nl_groups = -1;
-
-    /* NETLINK_KOBJECT_UEVENT: Get kernel messages to user space */
     int netlink_socket = socket(AF_NETLINK, SOCK_RAW, NETLINK_KOBJECT_UEVENT);
-    if (netlink_socket < 0) {
-        perror("socket");
-        return -1;
-    }
+    if (netlink_socket == -1)
+        goto cleanup;
 
-    if (bind(netlink_socket, (struct sockaddr *)&src_addr, sizeof(struct sockaddr_nl)) == -1) {
-        perror("bind");
-        close(netlink_socket);
-        return -1;
-    }
+    struct sockaddr_nl addr = {
+        .nl_family = AF_NETLINK,
+        .nl_pid = getpid(),
+        .nl_groups = -1
+    };
+
+    if (bind(netlink_socket, (struct sockaddr *)&addr, sizeof(struct sockaddr_nl)) == -1)
+        goto cleanup_socket;
 
     return netlink_socket;
+
+cleanup_socket:
+    close(netlink_socket);
+cleanup:
+    return -1;
 }
 
 
 static int read_netlink_socket(int netlink_socket)
 {
-    char buf[MSG_LEN] = {0};
-    struct iovec iov = { buf, sizeof(buf) };
+    char buf[4096] = {0};
     struct sockaddr_nl sa;
-    struct msghdr msg = { &sa, sizeof(sa), &iov, 1, NULL, 0, 0 };
+
+    struct iovec iov = {
+        .iov_base = buf,
+        .iov_len = sizeof(buf)
+    };
+
+    struct msghdr msg = {
+        .msg_name = &sa,
+        .msg_namelen = sizeof(sa),
+        .msg_iov = &iov,
+        .msg_iovlen = 1,
+        .msg_control = NULL,
+        .msg_controllen = 0,
+        .msg_flags = 0
+    };
 
     ssize_t len = recvmsg(netlink_socket, &msg, 0);
-
-    struct nlmsghdr *nh = (struct nlmsghdr *)buf;
-    while (NLMSG_OK(nh, len)) {
-
-        /* The end of multipart message. */
-        if (nh->nlmsg_type == NLMSG_DONE) {
-            printf("end netlink msg\n");
-            return 0;
-        }
-
-        if (nh->nlmsg_type == NLMSG_ERROR) {
-            /* Do some error handling. */
-            printf("error msg\n");
-        }
-
-        /* Continue here  with parsing payload. */
-        /* ... */
-        printf("Received message payload: %s\n", (char *)NLMSG_DATA(nh));
-
-        nh = NLMSG_NEXT(nh, len);
+    if (len == -1) {
+        perror("recvmsg");
+        return -1;
     }
+
+    printf("%s\n", buf);
 
     return 0;
 }
@@ -80,60 +74,23 @@ static int read_netlink_socket(int netlink_socket)
 
 static int set_poll_trigger(int netlink_socket)
 {
-    int ret = -1;
-    struct pollfd pfd = {0};
-
-    // msg and size_read variable must be removed
-    char msg[MSG_LEN] = {0};
-    ssize_t size_read = 0;
-
-    // set the poll on the netlink socket
-    pfd.fd = netlink_socket;
-    pfd.events = POLLIN;
+    struct pollfd pfd = {
+        .fd = netlink_socket,
+        .events = POLLIN,
+    };
 
     printf("Waiting for events ...\n");
     while (1) {
 
-        ret = poll(&pfd, 1, -1);
-        if (ret == -1) {
-            perror("poll()");
+        if (poll(&pfd, 1, -1) == -1) {
+            perror("poll");
             close(netlink_socket);
             return -1;
         }
 
-        if (pfd.revents & POLLIN) {
-            // TODO: replace recv by recvmsg
-//            read_netlink_socket(netlink_socket);
-            size_read = recv(netlink_socket, msg, sizeof(msg), MSG_DONTWAIT);
-            printf("size_read:%ld msg:%s\n", size_read, msg);
-        }
-
+        if (pfd.revents & POLLIN)
+            read_netlink_socket(netlink_socket);
     }
-
-    return 0;
-}
-
-
-static int entry()
-{
-    // set ctrl-c signal handler to exit the loop properly
-    struct sigaction action;
-
-    action.sa_handler = signal_handler;
-    if (sigaction(SIGINT, &action, NULL) == -1) {
-        perror("sigaction");
-        return -1;
-    }
-
-    // retrive netlink_socket
-    int netlink_socket = get_netlink_socket();
-    if (netlink_socket == -1)
-        return -1;
-    nl_socket_save = netlink_socket;
-
-    // set poll struct to trigger the read_netlink_socket
-    // function when the sockets receives data
-    set_poll_trigger(netlink_socket);
 
     return 0;
 }
@@ -141,7 +98,22 @@ static int entry()
 
 int main()
 {
-    entry();
+    struct sigaction action = {
+        .sa_handler = signal_handler,
+    };
+
+    if (sigaction(SIGINT, &action, NULL) == -1) {
+        perror("sigaction");
+        return -1;
+    }
+
+    int netlink_socket = get_netlink_socket();
+    if (netlink_socket == -1)
+        return -1;
+    nl_socket_save = netlink_socket;
+
+    set_poll_trigger(netlink_socket);
+
     return 0;
 }
 
